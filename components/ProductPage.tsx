7055507d007956f1283e0cf69133086755571c01
atetheone <atevirran@gmail.com>
2024-06-10 21:25:35 +0000

    Add products component+page

---
+++ components/ProductPage.tsx
@@ -0,0 +1,12 @@
+export default function ProductPage() {
+  return (
+    <div>
+      <h2>Our product list</h2>
+      <ul>
+        <li>Phones</li>
+        <li>TV</li>
+        <li>PC</li>
+      </ul>
+    </div>
+  );
+}
